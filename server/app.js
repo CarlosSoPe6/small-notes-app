6efc052e01ab11bc7cfd2bc0186dc2a780b8b80d
Carlos Soto <carlos_soto@epam.com>
2021-08-06 12:12:20 -0500

    Added authenticate middleware to notes and user router.

--- server/app.js
+++ server/app.js
@@ -4,12 +4,13 @@ import cors from 'cors';
 import authRouter from './routes/auth.route';
 import notesRouter from './routes/notes.route';
 import userRouter from './routes/user.route';
+import { authenticate } from './middlewares/authenticate.middleware';
 
 const app = express();
 app.use(cors());
 
 app.route('/api/auth', authRouter);
-app.route('/api/notes', notesRouter);
-app.route('/api/user', userRouter);
+app.route('/api/notes', authenticate, notesRouter);
+app.route('/api/user', authenticate, userRouter);
 
 export default app;
