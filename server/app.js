b881cc461d23171d83d3efac213ac81442211ce3
Carlos Soto <carlos_soto@epam.com>
2021-08-11 11:40:32 -0500

    Added missing import

--- server/app.js
+++ server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 
 const authRouter = require('./routes/auth.route');
 const notesRouter = require('./routes/notes.route');
