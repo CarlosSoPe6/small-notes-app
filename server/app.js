fe7ec9894cf7f6deaa1f88b002022109f179f6be
Carlos Soto <carlos_soto@epam.com>
2021-08-11 11:56:08 -0500

    Added wildcar route. The "catchall" handler: for any request that doesn't match one above, send back React's index.html file.

--- server/app.js
+++ server/app.js
@@ -7,14 +7,21 @@ const notesRouter = require('./routes/notes.route');
 const userRouter = require('./routes/user.route');
 const authenticate = require('./middlewares/authenticate.middleware');
 
+const STATIC_FILES_PATH = path.join(__dirname, 'public');
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(STATIC_FILES_PATH));
 
 app.use('/api/auth', authRouter);
 app.use('/api/notes', authenticate, notesRouter);
 app.use('/api/user', authenticate, userRouter);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(STATIC_FILES_PATH, 'index.html'));
+});
+
+console.log(STATIC_FILES_PATH);
 
 module.exports = app;
